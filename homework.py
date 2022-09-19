2f16845f77647b1c8e65360c0cc8e4a4b123e9b1
malkolmz <kurban.63.axmedov@gmail.com>
2022-09-19 15:47:19 +0300

    add 2 func

--- homework.py
+++ homework.py
@@ -1,22 +1,20 @@
 import psycopg2
 
-conn = psycopg2.connect(host='127.0.0.1', database='work_py_db', user='postgres', password='6336')
-# Вам необходимо разработать структуру БД для хранения информации и несколько функций на python для управления данными:
-#
+
 # Функция, создающая структуру БД (таблицы)
-with conn.cursor() as cur:
-    cur.execute("""
+def add_tables(cursor):
+    cursor.execute("""
                 DROP TABLE person;
                 DROP TABLE telephone;
                 """)
-    cur.execute("""
+    cursor.execute("""
                 CREATE TABLE IF NOT EXISTS telephone(
                  telephone_id SERIAL PRIMARY KEY,
-                 number DECIMAL UNIQUE
+                 number BIGINT UNIQUE
                  );
                  """)
 
-    cur.execute("""
+    cursor.execute("""
                 CREATE TABLE IF NOT EXISTS person(
                 person_id SERIAL PRIMARY KEY,
                 name VARCHAR(40) NOT NULL,
@@ -25,83 +23,108 @@ with conn.cursor() as cur:
                 telephone_id INTEGER REFERENCES telephone(telephone_id)
                 );
                 """)
-    conn.commit()
 
 
-    # Функция, позволяющая добавить нового клиента
-    def add_person(cursor, name: str, surname: str, email: str):
-        cursor.execute("""
-                    INSERT INTO person(name, surname, email)
-                    VALUES(%s, %s, %s);
-                    """, (name, surname, email))
+# Функция, позволяющая добавить нового клиента
+def add_person(cursor):
+    print('Введите данные клиента, которого желаете добавить:')
+    name = str(input('name:'))
+    surname = str(input('surname:'))
+    email = str(input('email:'))
+    cursor.execute("""
+                INSERT INTO person(name, surname, email)
+                VALUES(%s, %s, %s);
+                """, (name, surname, email))
 
 
-    add_person(cur, 'Kurban', 'Akhmedov', 'malkolm.63.zed@mail.ru')
-    conn.commit()
+# Функция, позволяющая добавить телефон для существующего клиента
+def add_telephone(cursor):
+    print('Введите данные клиента и номер телефона, который желаете добавить для клиента:')
+    name = str(input('name:'))
+    surname = str(input('surname:'))
+    number = str(input('number:'))
+
+    cursor.execute("""
+    INSERT INTO telephone(number)
+    VALUES( %s );""", (number,))
+    #
+    cursor.execute("""
+                    UPDATE person set telephone_id = (SELECT telephone_id FROM telephone WHERE number=%s ) 
+                    WHERE name=%s AND surname=%s ;
+                    """, (number, name, surname))
 
 
-    # Функция, позволяющая добавить телефон для существующего клиента
-    def add_telephone(cursor, number, name, surname):
-        cursor.execute("""
-        INSERT INTO telephone(number)
-        VALUES( %s );""", (number,))
-        #
-        cursor.execute("""
-                        UPDATE person set telephone_id = (SELECT telephone_id FROM telephone WHERE number=%s ) 
-                        WHERE name=%s AND surname=%s ;
-                        """, (number, name, surname))
+# Функция, позволяющая изменить данные о клиенте
+def update_person(cursor):
+    print('Введите данные клиента, которого желаете изменить:')
+    name = str(input('name:'))
+    surname = str(input('surname:'))
+    email = str(input('email:'))
 
-        conn.commit()
-
-
-    add_telephone(cur, '89640206389', 'Kurban', 'Akhmedov')
-
-
-    # Функция, позволяющая изменить данные о клиенте
-    def update_person(cursor, name: str, surname: str, mail: str):
-        pass
-
+    new_name = str(input('name:'))
+    new_surname = str(input('surname:'))
+    new_email = str(input('email:'))
 
     #
-    #     cursor.execute("""
-    #                     UPDATE person set
-    #                     """)
+    cursor.execute("""
+                    UPDATE person set name=%s, surname=%s, email=%s 
+                    WHERE name=%s AND surname=%s AND email=%s
+                    """, (new_name, new_surname, new_email, name, surname, email))
 
-    # Функция, позволяющая удалить телефон для существующего клиента
-    def del_telephone_date(cursor, name, surname):
-        cursor.execute("""
-                                    select person.telephone_id from
-                                    person join telephone on person.telephone_id =telephone.telephone_id
-                                    WHERE name=%s AND surname=%s;
-                                    """, (name, surname))
-        id_number = cur.fetchone()[0]
 
-        cursor.execute("""
-                        UPDATE person SET telephone_id=null WHERE name=%s AND surname=%s ;
-                        """, (name, surname))
+# Функция, позволяющая удалить телефон для существующего клиента
+def del_telephone_date(cursor):
+    print('Введите данные клиента, номер телефон которого желаете удалить:')
+    name = str(input('name:'))
+    surname = str(input('surname:'))
+    cursor.execute("""
+                                select person.telephone_id from
+                                person join telephone on person.telephone_id =telephone.telephone_id
+                                WHERE name=%s AND surname=%s;
+                                """, (name, surname))
+    id_number = cursor.fetchone()[0]
 
-        cursor.execute("""
-                    DELETE FROM telephone WHERE telephone_id=%s ; 
-                    """, (id_number,))
-        print(id_number)
+    cursor.execute("""
+                    UPDATE person SET telephone_id=null WHERE name=%s AND surname=%s ;
+                    """, (name, surname))
 
+    cursor.execute("""
+                DELETE FROM telephone WHERE telephone_id=%s ; 
+                """, (id_number,))
+
+
+# Функция, позволяющая удалить существующего клиента
+def del_person_date(cursor):
+    print('Введите данные клиента, которого желаете удалить')
+    name = str(input('name:'))
+    surname = str(input('surname:'))
+    cursor.execute("""
+                    DELETE FROM person WHERE name=%s AND surname=%s;
+                    """, (name, surname))
+
+
+# Функция, позволяющая найти клиента по его данным (имени, фамилии, email-у или телефону)
+def find_person_date(cursor):
+    print(
+        'Введите данные пользователя, которого хотите найти(данные в котором не уверенны можно отметить пропуском):')
+    name = str(input('name:'))
+    surname = str(input('surname:'))
+    email = str(input('email:'))
+    number = str(input('number:'))
+    print('Пользователи найденные по вашему запросу')
+    cursor.execute("""
+                    SELECT * FROM person LEFT JOIN telephone ON person.telephone_id = telephone.telephone_id
+                     WHERE name=%s OR surname=%s OR email=%s ;
+                    """, (name, surname, email, number))
+
+    return cursor.fetchone()
+
+
+# print(find_person_date(cur))
+
+if __name__ == '__main__':
+    conn = psycopg2.connect(host='127.0.0.1', database='work_py_db', user='postgres', password='6336')
+    with conn.cursor() as cur:
+        add_tables(cur)
         conn.commit()
-
-
-    # del_telephone_date(cur, 'Kurban', 'Akhmedov')
-
-    # Функция, позволяющая удалить существующего клиента
-    def del_person_date(cursor, name, surname):
-        cursor.execute("""
-                        DELETE FROM person WHERE name=%s AND surname=%s;
-                        """, (name, surname))
-
-        conn.commit()
-
-
-    # del_person_date(cur,'Kurban', 'Akhmedov')
-
-    # Функция, позволяющая найти клиента по его данным (имени, фамилии, email-у или телефону)
-    def find_person_date():
-        pass
-conn.close()
+    conn.close()
